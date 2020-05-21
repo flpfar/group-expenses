92f25da657ca88db253944e47b5d17f14a8feb16
Felipe Rosa <flp.far@gmail.com>
2020-05-21 19:33:43 -0300

    Remove unecessary lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ApplicationRecord
-  validates_uniqueness_of :username
   validates :username, presence: true, uniqueness: true
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,10 +2,4 @@ FactoryBot.define do
   factory :user do
     username { 'flpfar' }
   end
-
-  # factory :user do
-  #   first_name { "John" }
-  #   last_name  { "Doe" }
-  #   admin { false }
-  # end
 end
