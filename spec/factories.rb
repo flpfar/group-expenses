affa6362ab7ae918b1a6cd257afb0bcc3f7551dc
Felipe Rosa <flp.far@gmail.com>
2020-05-21 19:02:54 -0300

    Add user specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  factory :user do
+    username { 'flpfar' }
+  end
+
   # factory :user do
   #   first_name { "John" }
   #   last_name  { "Doe" }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'username should not be empty'
-  it 'username should be unique'
+  let(:user) { User.new }
+
+  it 'username should not be empty' do
+    user.valid?
+    expect(user.errors[:username]).to include("can't be blank")
+  end
+  it 'username should be unique' do
+    existing_user = create(:user)
+    user.username = existing_user.username
+    user.valid?
+    expect(user.errors[:username]).to include('has already been taken')
+  end
 end
