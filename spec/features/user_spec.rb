2a22c58fb1687f46b82b27384316bff305b61c9c
Felipe Rosa <flp.far@gmail.com>
2020-05-22 13:37:52 -0300

    Refactor fill_username_and_click on user spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,51 +6,51 @@ RSpec.feature 'Users', type: :feature do
     visit root_path
   end
 
-  def fill_username_and_click(username)
-    fill_in 'username', with: username
+  def fill_username_and_click(controller, username)
+    fill_in "#{controller}_username", with: username
     find("input[type='submit']").click
   end
 
   it 'login successfully' do
     create(:user, username: 'random')
-    fill_username_and_click('random')
+    fill_username_and_click('session', 'random')
     expect(page).to have_content('Welcome')
   end
 
   it 'login fails when user is not signed up' do
-    fill_username_and_click('random')
+    fill_username_and_click('session', 'random')
     expect(page).to have_content('Login failed')
   end
 
   it 'are redirected to profile page when login is sucessfull' do
     user = create(:user, username: 'random')
-    fill_username_and_click('random')
+    fill_username_and_click('session', 'random')
     expect(page).to have_current_path(user_path(user.id))
   end
 
   it 'root page redirects to profile when logged in' do
     user = create(:user, username: 'random')
-    fill_username_and_click('random')
+    fill_username_and_click('session', 'random')
     visit root_path
     expect(page).to have_current_path(user_path(user.id))
   end
 
   it 'signup successfully' do
     visit new_user_path
-    fill_username_and_click('felipe')
+    fill_username_and_click('user', 'felipe')
     expect(page).to have_content('Welcome')
   end
 
   it 'are redirected to profile page when signup is sucessfull' do
     visit new_user_path
-    fill_username_and_click('random')
+    fill_username_and_click('user', 'random')
     expect(page).to have_current_path(user_path(user.id))
   end
 
   it 'signup fails when username was already taken' do
     create(:user, username: 'felipe')
     visit new_user_path
-    fill_username_and_click('felipe')
+    fill_username_and_click('user', 'felipe')
     expect(page).to have_content('already taken')
   end
 end
